b4d5caaacf9b3a8cbe76eef8889af0b1ea0c2955
mtaufan3517 <mtaufan.3517@gmail.com>
2024-05-04 21:39:41 +0700

    ubah database dan view v_login

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
     public function index()
 	{
-		$this->load->view('halaman_utama'); 
+		$this->load->view('v_login'); 
 	}
 
 	public function simpan()
