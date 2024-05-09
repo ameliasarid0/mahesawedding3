50eeaba05c0e6c8f725f3ffc1e950f4c17d8335e
ameliasarid0 <ameliasarid0@gmail.com>
2024-05-10 03:49:24 +0700

    penambahan button login dan edit halaman utama

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,7 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
     public function index()
 	{
-		$this->load->view('v_login'); 
+		$this->load->view('halaman_utama'); 
+	}
+	public function login()
+	{
+		redirect(base_url('/login'));
 	}
 }
 
