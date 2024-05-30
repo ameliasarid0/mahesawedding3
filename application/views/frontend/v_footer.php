a255b02e7c1c032afcf7162a1511f0be7f69cecf
Azki Rafi Syahrir <azkirafisyahrir@gmail.com>
2024-05-30 20:09:43 +0700

    Penambahan footer dan header

---
+++ application/views/frontend/v_footer.php
@@ -0,0 +1,9 @@
+ <!-- Footer -->
+ <div id="formFooter">
+        Mau booking? Yuk <a class="underlineHover" href="#">Hubungi Admin</a>
+      </div>
+  
+    </div>
+    </div>
+</body>
+</html>
---
+++ application/views/frontend/v_header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>MAHESA WEDDING</title>
+  <link rel="stylesheet" href="<?php echo base_url() ?>css/style_login.css" />
+</head>
+<body align="center">
+    <div class="wrapper fadeInDown">
+    <div id="formcontent">
+      <!-- Icon -->
+      <div class="fadeIn first">
+        <img src="<?php echo base_url() ?>img/mahesa_logo.png" id="icon1" />
+      </div>
